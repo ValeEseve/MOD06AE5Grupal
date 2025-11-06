6fc2ed20f47d86eb96baad7fe21961f3aecac7e1
Vale Eseve <vale.eseve@gmail.com>
2025-11-06 19:58:04 -0300

    Primer commit!

---
+++ AE5grupal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AE5grupal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AE5grupal.settings')
+
+application = get_asgi_application()
